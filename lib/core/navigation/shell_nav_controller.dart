9c87e91957f915e67e55305249a3b79a17cf301f
MichMatrix <michel.a.s401@gmail.com>
2026-02-19 23:57:43 -0300

    feat: add nav to assist menu shell

---
+++ lib/core/navigation/shell_nav_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class ShellNavController extends ChangeNotifier {
+  int _index = 0;
+  int get index => _index;
+
+  void setIndex(int newIndex) {
+    if (newIndex == _index) return;
+    _index = newIndex;
+    notifyListeners();
+  }
+}
