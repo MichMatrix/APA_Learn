9eacef1e441429b666a0b1b507cc880cf75ae5e1
MichMatrix <michel.a.s401@gmail.com>
2026-02-19 23:56:28 -0300

    feat: add app initial config
    config rout generate and local language setting

---
+++ lib/app/app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import '../app/routes/route_generator.dart';
+import '../core/settings/app_settings_controller.dart';
+import '../l10n/app_localizations.dart';
+import '../core/navigation/shell_nav_controller.dart';
+import '../features/splash/presentation/splash_screen.dart';
+
+class APALearnApp extends StatelessWidget {
+  final AppSettingsController settings;
+  final ShellNavController nav;
+
+  const APALearnApp({
+    super.key,
+    required this.settings,
+    required this.nav,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: settings,
+      builder: (_, __) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: RouteGenerator.generateRoute,
+
+          locale: settings.locale,
+          supportedLocales: const [Locale('pt'), Locale('en')],
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+
+          home: SplashScreen(settings: settings, nav: nav),
+        );
+      },
+    );
+  }
+}
